528514a5938c816b64ce89d7156422a20ee698ef
Jeancarlos ML <jeancarlosmamanilingan@gmail.com>
2020-09-23 12:30:16 -0500

    save

--- index.php
+++ index.php
@@ -15,34 +15,6 @@
             <div class="nav-logo">
                 <img src="imagenes/logo.jpg" alt="">
             </div>
-<<<<<<< HEAD
-            <nav>
-                <ul>
-                    <li><a href="">Inicio</a></li>
-                    <li><a href="tienda/coleccion.php">Tienda</a></li>
-                    <li><a href="">Quienes Somos</a></li>
-                    <li><a href="">Contacto</a></li>
-                    <li><a href="controller/LoginController.php?action=login"><i class="fas fa-user" ></i></a></li>
-                    <li><a href=""><i class="fas fa-cart-plus"></i></a></li>
-
-                </ul>
-            </nav>
-
-        </div>
-
-        <div class="fondo">
-            <div class="texto-fondo">
-                <h1>Compra ya !!!</h1>
-            </div>
-        </div>
-
-        <footer>
-            <a href=""><i class="fas fa-facebook-square">Facebook</i></a>
-            <a href=""><i class="fas fa-twitter" >Twitter</i></a>
-            <a href=""><i class="fas fa-instagram" >Instagram</i></a>
-            <p>Derechos reservados por Neonled-store</p>
-        </footer>
-=======
             <ul class="nav-list">
                 <li><a href="">Inicio</a></li>
                 <li><a href="">Productos</a></li>
@@ -57,7 +29,6 @@
                 <h1>NEON LED<br> STORE</h1>
             </header>
         </main>
->>>>>>> 7a303a6f9fd76beb30f7e9bf0927003f00fee853
     </div>
 </body>
 
