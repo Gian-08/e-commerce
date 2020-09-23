7a303a6f9fd76beb30f7e9bf0927003f00fee853
Jeancarlos ML <jeancarlosmamanilingan@gmail.com>
2020-09-23 10:31:01 -0500

    stylos a index

--- index.php
+++ index.php
@@ -11,36 +11,24 @@
 
 <body>
     <div class="contenedor">
-        <div class="navbar">
-            <div class="logo">
+        <nav class="nav">
+            <div class="nav-logo">
                 <img src="imagenes/logo.jpg" alt="">
             </div>
-            <nav>
-                <ul>
-                    <li><a href="">Inicio</a></li>
-                    <li><a href="">Productos</a></li>
-                    <li><a href="">Quienes Somos</a></li>
-                    <li><a href="">Contacto</a></li>
-                    <li><a href="controller/LoginController.php?action=login"><i class="fas fa-user" ></i></a></li>
-                    <li><a href=""><i class="fas fa-cart-plus"></i></a></li>
-
-                </ul>
-            </nav>
-
-        </div>
-
-        <div class="fondo">
-            <div class="texto-fondo">
-                <h1>Compra ya !!!</h1>
-            </div>
-        </div>
-
-        <footer>
-            <a href=""><i class="fas fa-facebook-square">Facebook</i></a>
-            <a href=""><i class="fas fa-twitter" >Twitter</i></a>
-            <a href=""><i class="fas fa-instagram" >Instagram</i></a>
-            <p>Derechos reservados por Neonled-store</p>
-        </footer>
+            <ul class="nav-list">
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Quienes Somos</a></li>
+                <li><a href="">Contacto</a></li>
+                <li><a href="controller/LoginController.php?action=login"><i class="fas fa-user"></i></a></li>
+                <li><a href=""><i class="fas fa-cart-plus"></i></a></li>
+            </ul>
+        </nav>
+        <main class="main">
+            <header class="main-header">
+                <h1>NEON LED<br> STORE</h1>
+            </header>
+        </main>
     </div>
 </body>
 
